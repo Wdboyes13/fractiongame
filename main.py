e671e5b5e3a54f056ee3113ac4d83f56310da8b8
Wdboyes13 <modes-recheck-0r@icloud.com>
2025-02-18 11:06:07 -0800

    update main

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import random
+operations = ["x", "/", "+", "-"]
+oprnum = random.randrange(0, 3)
+print(random.randrange(1, 30), "",  random.randrange(1, 30))
+print("-", operations[oprnum], "-")
+print(random.randrange(1, 30), "",  random.randrange(1, 30))
 
